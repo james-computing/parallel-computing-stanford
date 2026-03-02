dca9fa9e055459c2839ecffd5e83c400b65b1b6c
james <jamessimeao@outlook.com>
2026-03-01 22:02:19 -0300

    Remove code from main and call lec2::execute_examples instead

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,9 @@
 #include <iostream>
-//#include <cmath>
-#include <chrono>
 
 #include "../include/lec2.hpp"
 
 int main() {
-    // Alloc resources
-    size_t const N {100};
-    size_t const terms {10};
-    float * x = new float[N];
-    float * y = new float[N];
-    float * sin = new float[N];
-
-    // Initialize x
-    for (std::size_t i {0}; i < N; ++i) {
-        x[i] = 0.1* (i % 10);
-    }
-
-    // Variables to measure time
-    std::chrono::steady_clock::time_point begin;
-    std::chrono::steady_clock::time_point end;
-
-    // Compute sin(x[i]) for each i, and output the results to y
-    begin = std::chrono::steady_clock::now();
-    lec2::sinx(N, terms, x, y);
-    end = std::chrono::steady_clock::now();
-    
-    std::cout << "time sinx: " << std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count() << std::endl;
-
-    // Compute again, but with 2 threads
-    begin = std::chrono::steady_clock::now();
-    lec2::parallel_sinx(N, terms, x, y);
-    end = std::chrono::steady_clock::now();
-
-    std::cout << "time parallel_sinx: " << std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count() << std::endl;
-
-    // Cleanup
-    delete[] sin;
-    delete[] y;
-    delete[] x;
-    sin = nullptr;
-    y = nullptr;
-    x = nullptr;
+    lec2::execute_examples();
 
     return 0;
 }
